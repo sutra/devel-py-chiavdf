b5538b5ff831fed96eef0aefa16fac13888a6bbb
Charlie Root <root@chia.oxerr.net>
2021-09-26 15:18:48 +0000

    Update patch-setup.py.

--- files/patch-setup.py
+++ files/patch-setup.py
@@ -1,11 +1,7 @@
 --- setup.py.orig	2021-08-26 20:29:35 UTC
 +++ setup.py
-@@ -73,10 +73,10 @@ def copy_vdf_bench(build_dir, install_dir):
- 
- 
- def invoke_make(**kwargs):
--    subprocess.check_output("make -C src -f Makefile.vdf-client", shell=True)
-+    subprocess.check_output("gmake -C src -f Makefile.vdf-client", shell=True)
+@@ -76,7 +76,7 @@ def invoke_make(**kwargs):
+     subprocess.check_output("make -C src -f Makefile.vdf-client", shell=True)
  
  
 -BUILD_VDF_CLIENT = os.getenv("BUILD_VDF_CLIENT", "Y") == "Y"
@@ -13,28 +9,15 @@
  BUILD_VDF_BENCH = os.getenv("BUILD_VDF_BENCH", "N") == "Y"
  
  
-@@ -167,6 +167,7 @@ ext_modules = [
-         ),
-         include_dirs=[
-             # Path to pybind11 headers
-+            "/usr/local/include",
+@@ -170,6 +170,7 @@ ext_modules = [
              get_pybind_include(),
              get_pybind_include(user=True),
              "mpir_gc_x64",
-@@ -221,6 +222,12 @@ class BuildExt(build_ext):
-         "unix": [""],
-     }
- 
-+    if "WITH_DEBUG" in os.environ:
-+        c_opts["unix"] += ["-g"]
-+        l_opts["unix"] += ["-g"]
-+    else:
-+        l_opts["unix"] += ["-s"]
-+
-     if sys.platform == "darwin":
-         darwin_opts = ["-stdlib=libc++", "-mmacosx-version-min=10.14"]
-         c_opts["unix"] += darwin_opts
-@@ -232,7 +239,8 @@ class BuildExt(build_ext):
++            "/usr/local/include",
+         ],
+         library_dirs=["mpir_gc_x64"],
+         libraries=["mpir"],
+@@ -232,7 +233,8 @@ class BuildExt(build_ext):
          link_opts = self.l_opts.get(ct, [])
          if ct == "unix":
              opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
@@ -44,7 +27,7 @@
              if has_flag(self.compiler, "-fvisibility=hidden"):
                  opts.append("-fvisibility=hidden")
          elif ct == "msvc":
-@@ -262,7 +270,6 @@ if platform.system() == "Windows":
+@@ -262,7 +264,6 @@ if platform.system() == "Windows":
      )
  else:
      build.sub_commands.append(("build_hook", lambda x: True))  # type: ignore
@@ -52,7 +35,7 @@
  
      setup(
          name="chiavdf",
-@@ -274,11 +281,12 @@ else:
+@@ -274,11 +275,12 @@ else:
          long_description=open("README.md").read(),
          long_description_content_type="text/markdown",
          url="https://github.com/Chia-Network/chiavdf",
